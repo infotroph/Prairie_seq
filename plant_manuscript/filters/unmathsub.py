496a4c8193e9951e2bb948cf2b0e8da12fc3c894
Chris Black <chris@ckblack.org>
2016-11-29 02:04:07 -0500

    update citation style and unmathsub script from rhizotron repo
    unmathsub now unmaths superscripts as well as subscripts. ecology.csl now displays DOI if an article has no page/volume number.

--- plant_manuscript/filters/unmathsub.py
+++ plant_manuscript/filters/unmathsub.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 '''
-Pandoc filter to convert inline math subscripts to text sbscripts.
+Pandoc filter to convert inline math sub/superscripts to text sub/superscripts.
 Written for a very specific problem:
 	Bibtex entries with "CO_{2}" are rendered by the Pandoc parser as
 	[Str "CO",Math InlineMath "_{2}"],
@@ -12,8 +12,9 @@ This filter solves this problem by replacing
 with a text subscript:
 	[Subscript Str "..."],
 thus removing the equation field entirely.
+Have now extended to do the same for superscripts, e.g. "^{15}N"
 
-Use with caution -- will probably break real inline equations that start with a subscript.
+Use with caution -- will probably break real inline equations that start with a sub- or superscript.
 
 To use this on bibliography entries, this filter needs to be downstream of pandoc-citeproc.
 I do this by running pandoc twice: Once for pandoc-citeproc with native output, then pipe that output into the second run for this filter, e.g.
@@ -29,7 +30,7 @@ I do this by running pandoc twice: Once for pandoc-citeproc with native output, 
 		-o output.docx
 '''
 
-from pandocfilters import toJSONFilter, Str, Subscript
+from pandocfilters import toJSONFilters, Str, Subscript, Superscript
 
 def unmathsub(key, value, format, meta):
 	if (key == 'Math'
@@ -41,5 +42,15 @@ def unmathsub(key, value, format, meta):
 					x = Str(x.split('_{')[1].split('}')[0])
 					return Subscript([x])
 
+def unmathsup(key, value, format, meta):
+	if (key == 'Math'
+		and len(value) == 2
+		and '^{' in (x[:2] for x in value if isinstance(x, str))
+		and 'InlineMath' in (x['t'] for x in value if 't' in x)):
+			for x in value:
+				if isinstance(x, str):
+					x = Str(x.split('^{')[1].split('}')[0])
+					return Superscript([x])
+
 if __name__ == "__main__":
-    toJSONFilter(unmathsub)
+    toJSONFilters([unmathsub, unmathsup])
